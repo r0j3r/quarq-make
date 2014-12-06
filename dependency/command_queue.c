2d825f637f3749894e6c3f76f52aa66796863139
rogelio <rogelio.serrano@gmail.com>
2014-12-06 13:52:07 +0000

    style changes

--- dependency/command_queue.c
+++ dependency/command_queue.c
@@ -5,14 +5,12 @@
 #include "queue.h"
 #include "dep.h"
 
-struct job
-{
+struct job {
     struct node * d;
     unsigned char out_of_date;
 };
 
-struct job_queue 
-{
+struct job_queue {
     struct job_queue * n;
     short t;
     short h;
@@ -75,20 +73,17 @@ next_job() {
 }
 
 int
-execute_command_queue()
-{
+execute_command_queue() {
     
 }
 
-struct run_item
-{
+struct run_item {
     int pid;
     void * dep;
 };
 
 void *
-create_run_item(void * i, int pid)
-{
+create_run_item(void * i, int pid) {
     struct run_item * r;
 
     r = malloc(sizeof(*r));
@@ -99,76 +94,64 @@ create_run_item(void * i, int pid)
 }
 
 int
-start_job(void * run_q, void * i)
-{
+start_job(void * run_q, void * i) {
     int pid; 
     void * d;
 
     d = get_data(i);
     pid = run_commands(d);
-    if (pid > 0)
-    {
+    if (pid > 0) {
         void * r_i;
         r_i = create_run_item(i, pid);
         insert_in_q(run_q, r_i);
-    }
-    else 
+    } else { 
         return -1;
+    } 
 }
 
 int
-run_commands(void * d)
-{
+run_commands(void * d) {
     int pid;
     
     pid = fork();
-    if (pid)
-    {
+    if (pid) {
         void * i;
         void * c;
 
         i = create_iterator(get_commands(d));
         c = next_item(i);
-        while(c)
-        {
+        while(c) {
             pid = fork();
-            if (pid)
-            {
+            if (pid) {
                 wait();
                 c = next_item(i);
-            }
-            else
+            } else {
                 exec();
+            }
         }
-    }
-    else
-    {
+    } else {
         wait();
     }
 }
 
 int
-clean_up_job(int pid)
-{
+clean_up_job(int pid) {
     void * d;
     d = remove_from_run_q(pid);
     update_dependents(d);
 }
 
 int
-update_dependents(void * d)
-{
+update_dependents(void * d) {
     void * i;
     void * v;
     i = create_dep_iter(get_dependents_list(d));
     v = next_dep(i);
-    while(v)
-    {
+    while(v) {
         void * data;
         data = get_data(v);
         dec_wait_count(data);
-        if (done_waiting(data))
-        {
+        if (done_waiting(data)) {
             insert_in_q(ready, v);
         }
         v = next_dep(i);
